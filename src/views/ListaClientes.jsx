0bda78dfaf111aba2a42561f3be490af6033439a
Brenda Galan <brendagalan4@gmail.com>
2026-06-27 00:04:27 -0300

    Agregue barra de busqueda y la logica para filtrar por apellido o ciudad

--- src/views/ListaClientes.jsx
+++ src/views/ListaClientes.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import { Box, CircularProgress, Alert, Typography } from '@mui/material';
+import { Box, CircularProgress, Alert, Typography, TextField, InputAdornment} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 function ListaClientes() {
   const [clientes, setClientes] = useState([]);
@@ -48,9 +49,47 @@ function ListaClientes() {
         );
     }
 
+//Filtro por apellido o ciudad
+const clientesFiltrados = clientes.filter((cliente) => {
+      const apellido = cliente.name.lastname.toLowerCase();
+      const ciudad = cliente.address.city.toLowerCase();
+      const busquedaLower = busqueda.toLowerCase();
+
+      return apellido.includes(busquedaLower) || ciudad.includes(busquedaLower);
+    });
+    
 return (
         <Box sx={{ p: { xs: 2, md: 4 }, maxWidth: '1400px', margin: '0 auto' }}>
-            Lista Clientes
+            <Box sx={{ mb: 4, borderBottom: '2px solid #f1f5f9', pb: 2 }}>
+                <Typography variant="h4" component="h2" sx={{ fontWeight: 700, color: '#0f172a', letterSpacing: '-0.5px' }}>
+                    Listado de Clientes
+                </Typography>
+                <Typography variant="body2" sx={{ color: '#64748b', mt: 0.5 }}>
+                    Visualizá y filtrá los usuarios registrados en el sistema central.
+                </Typography>
+            </Box>
+
+            {/* Barra de Búsqueda*/}
+            <Box sx={{ mb: 3 }}>
+                <TextField
+                    placeholder="Buscar por apellido o ciudad..."
+                    variant="outlined"
+                    size="small"
+                    value={busqueda}
+                    onChange={(e) => setBusqueda(e.target.value)}
+                    sx={{ 
+                        width: '100%', maxWidth: '450px', backgroundColor: '#ffffff',
+                        '& .MuiOutlinedInput-root': { borderRadius: '8px', '&:hover fieldset': { borderColor: '#1976d2' } }
+                    }}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon sx={{ color: '#94a3b8' }} />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+            </Box>
         </Box>
     );
 }
