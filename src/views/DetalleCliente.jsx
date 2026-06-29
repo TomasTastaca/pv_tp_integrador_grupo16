a82d28d19326c4bff9a6a9b745bb46cdff71fb3c
tastaca gutierrez tomas leandro <tastacaleandro@gmail.com>
2026-06-28 23:55:17 -0300

    renderize la ficha completa del cliente con datos anidados

--- src/views/DetalleCliente.jsx
+++ src/views/DetalleCliente.jsx
@@ -1,4 +1,7 @@
-import { Paper, Typography , Box, CircularProgress, Alert } from "@mui/material";
+import { Paper, Typography , Box, CircularProgress, Alert ,
+  Grid,
+  Divider
+} from "@mui/material";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
@@ -47,15 +50,124 @@ if (error) {
    );
 }
   return (
-    <Paper elevation={3} sx={{ padding: 3 }}>
-      <Typography variant="h4" component="h2" gutterBottom>
-        Detalle del Cliente
+    <>
+     
+    <Paper 
+     elevation={4}
+      sx={{
+        maxWidth: 900,
+        mx: "auto",
+        mt: 4,
+        p: 4,
+        borderRadius: 3,
+      }}
+    > 
+      <Typography
+        variant="h4"
+        color="primary"
+        fontWeight="bold"
+        gutterBottom
+        sx={{ textAlign: "center" }}
+      >
+        Ficha Completa del Cliente de {cliente.name.firstname} {cliente.name.lastname} 
       </Typography>
 
-      <Typography variant="body1">
-        En esta pantalla se mostrará la ficha completa del cliente.
+      <Divider sx={{ mb: 3 }} />
+
+      {/* Información Personal */}
+      <Typography
+        variant="h6"
+        sx={{ mb: 2, color: "#1976d2", fontWeight: "bold", fontSize: "1.5rem" }}
+      >
+        Información Personal
       </Typography>
+      
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Nombre:</strong></Typography>
+          <Typography>
+            {cliente.name.firstname}
+          </Typography>
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Apelllido:</strong></Typography>
+          <Typography>{cliente.name.lastname}</Typography>
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Email:</strong></Typography>
+          <Typography>{cliente.email}</Typography>
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Teléfono:</strong></Typography>
+          <Typography>{cliente.phone}</Typography>
+        </Grid>
+      </Grid>
+
+      <Divider sx={{ my: 3 }} />
+
+      {/* Credenciales */}
+      <Typography
+        variant="h6"
+        sx={{ mb: 2, color: "#1976d2", fontWeight: "bold", fontSize: "1.5rem" }}
+      >
+        Credenciales de Acceso de la Base de Datos
+      </Typography>
+
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Usuario:</strong></Typography>
+          <Typography>{cliente.username}</Typography>
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Contraseña:</strong></Typography>
+          <Typography>{cliente.password}</Typography>
+        </Grid>
+      </Grid>
+
+      <Divider sx={{ my: 3 }} />
+
+      {/* Dirección */}
+      <Typography
+        variant="h6"
+        sx={{ mb: 2, color: "#1976d2", fontWeight: "bold", fontSize: "1.5rem" }}
+      >
+        Dirección Completa
+      </Typography>
+
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Calle:</strong></Typography>
+          <Typography>{cliente.address.street}</Typography>
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Número:</strong></Typography>
+          <Typography>{cliente.address.number}</Typography>
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Ciudad:</strong></Typography>
+          <Typography>{cliente.address.city}</Typography>
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Código Postal:</strong></Typography>
+          <Typography>{cliente.address.zipcode}</Typography>
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Latitud:</strong></Typography>
+          <Typography>{cliente.address.geolocation.lat}</Typography>
+        </Grid>
+
+      <Grid size={{ xs: 12, md: 6 }}>
+          <Typography><strong>Longitud:</strong></Typography>
+          <Typography>{cliente.address.geolocation.long}</Typography>
+        </Grid>
+      </Grid>
     </Paper>
+    </>
   );
 };
 
